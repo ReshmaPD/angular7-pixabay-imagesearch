805ba2bbcbe7348feaa924635358d88fd4e035fc
Reshma D <developerreshmad@gmail.com>
2019-03-21 19:47:53 +0530

    app module import material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+
+
+import { ImageService } from './shared/image.service';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '././material.module';
+// import {MatCardModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
+// import { MasonryModule } from 'angular2-masonry';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    // MasonryModule,
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
+
+// @NgModule({
+//   imports: [MatCardModule, MatButtonModule, MatCheckboxModule],
+//   exports: [MatCardModule, MatButtonModule, MatCheckboxModule],
+// })
+
+
+
 export class AppModule { }
