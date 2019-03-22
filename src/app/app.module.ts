2ad4ea37c3960dc6ba19c561c3671876189c0073
Reshma D <developerreshmad@gmail.com>
2019-03-22 14:01:49 +0530

    added router and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { DeferLoadModule } from '@trademe/ng-defer-load';
 
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './image-list/image-list.component';
@@ -12,31 +14,36 @@ import { ImageService } from './shared/image.service';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from '././material.module';
-// import {MatCardModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
-// import { MasonryModule } from 'angular2-masonry';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 
+const appRoutes: Routes = [
+     // basic routes
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: ImageListComponent },
+    { path: 'user-info', component: UserInfoComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
-    ImageListComponent
+    ImageListComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
+    DeferLoadModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule,
-    // MasonryModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [ImageService],
   bootstrap: [AppComponent]
 })
-
-// @NgModule({
-//   imports: [MatCardModule, MatButtonModule, MatCheckboxModule],
-//   exports: [MatCardModule, MatButtonModule, MatCheckboxModule],
-// })
 
 
 
